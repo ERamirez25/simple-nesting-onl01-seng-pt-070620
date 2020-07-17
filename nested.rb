8e9657ddcb63a72851952b6dcda866e4f6bf49eb
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-17 00:06:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:changing_alan][:known_for] = "GUI"
+     programmer_hash[:changing_alan][:alan_kay][:known_for] = "GUI"
      return programmer_hash
    
      
